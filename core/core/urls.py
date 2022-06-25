5d9a0bd02895b2b40064fb5a1bdac35e2d18b49f
ProfNuru <nurudeen.karim2016@gmail.com>
2022-06-25 23:30:35 +0000

    documentation created for api endpoints on route /docs

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
@@ -11,4 +13,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('docs/', include_docs_urls(title='HackathonAPI')),
+    path('schema/', get_schema_view(
+        title="HackathonAPI",
+        description="API for the GHN Hackathon Project",
+        version="1.0.0",
+    ), name="openapi-schema"),
 ]
