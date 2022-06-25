e13acad98f6bff157459330d66f12ca21b82824b
ProfNuru <nurudeen.karim2016@gmail.com>
2022-06-25 15:14:58 +0000

    profile and hub models created

--- core/users/models.py
+++ core/users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+# Profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    contact = models.CharField(max_length=20, null=True, blank=True)
+    physical_address = models.TextField(null=True, blank=True)
+    gps_address = models.CharField(max_length=15, null=True, blank=True)
+    about = models.TextField(null=True, blank=True)
+    # profile_pic
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+
+# Hub
+class Hub(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    hub_name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    contact = models.CharField(max_length=20, null=True, blank=True)
+    physical_address = models.TextField(null=True, blank=True)
+    gps_address = models.CharField(max_length=15, null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    business_registration_number = models.CharField(max_length=100)
+    # business certificate pic or pdf
+
+    def __str__(self):
+        return self.hub_name
+
+
