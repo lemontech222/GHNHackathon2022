283d111b72ff4be7cb8ef334c38cf496e6291a32
ProfNuru <nurudeen.karim2016@gmail.com>
2022-07-27 21:50:31 +0000

    nav bar complete

--- frontend/src/components/Navigation/Nav.jsx
+++ frontend/src/components/Navigation/Nav.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faSearch} from '@fortawesome/free-solid-svg-icons'
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 import {NavBar} from './Nav.style.js';
 import appLogo from '../../images/logo/ghnlogo.png';
 
 const Nav = () => {
+  const navigate = useNavigate()
+
+  const goToHomepage = ()=>{
+    navigate('/')
+  }
+  
   return (
     <div style={{width:'100%',backgroundColor:'#fff',borderBottom:'3px solid #555'}}>
       <Container>
         <NavBar>
           <div className="app-logo">
-            <img src={appLogo} alt="Ghana Hubs Network Logo" />
+            <img onClick={goToHomepage} src={appLogo} alt="Ghana Hubs Network Logo" />
           </div>
           <form className="search-form">
             <input type="text" placeholder="Search" />
@@ -22,8 +28,22 @@ const Nav = () => {
             </button>
           </form>
           <div className="navLinks">
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
+            <Link to="/login" style={{
+              textDecoration:'none',
+              color:'#000',
+              fontWeight:'bolder',
+              display:'inline-block',
+              marginRight:'5px',
+            }}>Login</Link>
+            <Link to="/register" style={{
+              textDecoration:'none',
+              color:'#fff',
+              fontWeight:'bolder',
+              backgroundColor:'#34ABDE',
+              padding:'5px 8px',
+              marginLeft:'5px',
+              borderRadius:'5px'
+            }}>Register</Link>
           </div>      
         </NavBar>
       </Container>
