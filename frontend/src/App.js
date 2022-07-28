1df20351bdc287fdd67e6ca05b4a81e763a44091
ProfNuru <nurudeen.karim2016@gmail.com>
2022-07-28 08:14:21 +0000

    dashboard routes functional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Login from "./components/pages/Login/Login"
 import Register from "./components/pages/Register/Register"
 import Hub from "./components/pages/Hub/Hub"
 import Startup from "./components/pages/Startup/Startup"
+import Dashboard from "./components/pages/Dashboard/Dashboard";
 import { useNavbar } from "./context/NavigationsContext";
 
 import Container from 'react-bootstrap/Container';
@@ -22,6 +23,7 @@ function App() {
             <Route path="login" element={<Login />} />
             <Route path="register" element={<Register />} />
             <Route path="dashboard">
+              <Route path="" element={<Dashboard />} />
               <Route path="hub" element={<Hub />} />
               <Route path="startup" element={<Startup />} />
             </Route>
--- frontend/src/components/pages/Register/Register.style.js
+++ frontend/src/components/pages/Register/Register.style.js
@@ -2,10 +2,15 @@ import styled from 'styled-components';
 
 export const DesgnationButtons = styled.button`
     &{
+        width:100px;
         padding:15px;
         border: 1px solid ${(props)=>props.designation.set ? 'var(--primary-color)' : '#ccc'};
+        border-top-left-radius: ${(props)=>props.designation.value==='register_hub' ? '10px' : '0px'};
+        border-bottom-left-radius: ${(props)=>props.designation.value==='register_hub' ? '10px' : '0px'};
+        border-top-right-radius: ${(props)=>props.designation.value==='register' ? '10px' : '0px'};
+        border-bottom-right-radius: ${(props)=>props.designation.value==='register' ? '10px' : '0px'};
         background-color: ${(props)=>props.designation.set ? 'var(--primary-color)' : '#ccc'};
-        color:${(props)=>props.designation.set ? '#ccc' : 'var(--primary-color)'};
+        color:${(props)=>props.designation.set ? '#fff' : '#333'};
         transition:0.4s all;
     }
     &:hover{
