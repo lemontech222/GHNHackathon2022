e752696da9e0da7f4ce340144c362181024787e2
ProfNuru <nurudeen.karim2016@gmail.com>
2022-07-28 12:26:23 +0000

    save

--- frontend/src/components/Navigation/Nav.jsx
+++ frontend/src/components/Navigation/Nav.jsx
@@ -7,13 +7,19 @@ import {Link, useNavigate} from 'react-router-dom';
 import {NavBar} from './Nav.style.js';
 import appLogo from '../../images/logo/ghnlogo.png';
 import { useTokens } from '../../context/TokensContext.js';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 const Nav = ({show}) => {
   const navigate = useNavigate()
-  const {tokens,setTokens} = useTokens()
+  const {tokens,setTokens,user} = useTokens()
 
   const goToHomepage = ()=>{
     navigate('/')
+  }
+
+  const toDashboard = () =>{
+    navigate('/dashboard')
   }
 
   const logOut = ()=>{
@@ -61,22 +67,34 @@ const Nav = ({show}) => {
               }}>Register</Link>
             </>}
             {tokens && <>
-              <Link onClick={logOut} to="" style={{
-                textDecoration:'none',
-                color:'#000',
-                fontWeight:'bolder',
-                display:'inline-block',
-                marginRight:'5px',
-              }}>Logout</Link>
-              <Link to="/dashboard" style={{
-                textDecoration:'none',
-                color:'#fff',
-                fontWeight:'bolder',
-                backgroundColor:'#34ABDE',
-                padding:'5px 8px',
-                marginLeft:'5px',
-                borderRadius:'5px'
-              }}>Dashboard</Link>
+              <Dropdown>
+                <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                  <img style={{height:'30px'}} src={user?.profile_pic} alt="profile pic" /> {user?.username}
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="">
+                    <p onClick={logOut} to="" style={{
+                      textDecoration:'none',
+                      color:'#000',
+                      fontWeight:'bolder',
+                      display:'inline-block',
+                      marginRight:'5px',
+                    }}>Logout</p>
+                  </Dropdown.Item>
+                  <Dropdown.Item href="">
+                  <p onClick={toDashboard} style={{
+                    textDecoration:'none',
+                    color:'#000',
+                    fontWeight:'bolder',
+                    display:'inline-block',
+                    marginRight:'5px',
+                  }}>Dashboard</p>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              
+              
             </>}        
           </div>      
         </NavBar>
