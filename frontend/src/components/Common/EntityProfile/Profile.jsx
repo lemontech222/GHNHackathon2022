0c34938dab12e032df326637e75c6b9751ba7d3a
mozay <mosesanao13@gmail.com>
2022-07-28 02:27:09 +0000

    Common profile component created

---
+++ frontend/src/components/Common/EntityProfile/Profile.jsx
@@ -0,0 +1,32 @@
+import { AnchorLink } from '..';
+import { ProfileWrapper } from './Profile.styles';
+
+const EntityProfile = () => {
+  return (
+    <ProfileWrapper>
+      <div className="hero-section">
+        <div className="profile-photo"></div>
+      </div>
+      <div className="profile-name">
+        <h2>Fab Hub Ashanti</h2>
+      </div>
+      <div className="info-section">
+        <div className="info">
+          <h3>Communities</h3>
+          <h3>51</h3>
+        </div>
+        <div className="info">
+          <h3>Innovators/Enterpreneurs</h3>
+          <h3>588</h3>
+        </div>
+        <div className="action">
+          <AnchorLink to="">
+            <p>View Profile</p>
+          </AnchorLink>
+        </div>
+      </div>
+    </ProfileWrapper>
+  );
+};
+
+export default EntityProfile;
---
+++ frontend/src/components/Common/EntityProfile/Profile.styles.jsx
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+
+export const ProfileWrapper = styled.div`
+  width: 100%;
+  background: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0px 1px 0px 0px #cac1c1;
+  display: flex;
+  flex-direction: column;
+  align-items: ${({ align }) => (align ? align : 'flex-start')};
+  margin-bottom: 50px;
+
+  & .hero-section {
+    width: 100%;
+    height: 180px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+    margin-bottom: 100px;
+    display: flex;
+    justify-content: center;
+    background: var(--primary-color);
+  }
+
+  & .profile-photo {
+    position: relative;
+    width: 180px;
+    min-height: 120px;
+    top: 50%;
+    background: #ffffff;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  & .profile-name {
+    width: 100%;
+    text-align: center;
+
+    & h2 {
+      font-style: normal;
+      font-weight: 600;
+      font-size: 22px;
+      line-height: 17px;
+      color: var(--primary-color);
+    }
+  }
+
+  & .info-section {
+    width: 100%;
+    height: 260px;
+    background: #ffffff;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  & .info {
+    width: 100%;
+    height: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid rgba(72, 72, 72, 0.2);
+
+    & h3 {
+      font-style: normal;
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 17px;
+      color: #000000;
+    }
+  }
+
+  & .action {
+    width: 100%;
+    padding: 20px 0;
+    text-align: center;
+    // border-bottom-radius: 15px;
+
+    & p {
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 18px;
+      color: var(--primary-color);
+    }
+  }
+`;
